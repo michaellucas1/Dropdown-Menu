b9d32b015ab50eef8302c8e90af0089671033797
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-10-21 20:04:51 -0400

    add import for dropdown modules and add test driver

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import "./styles/index.css";
 import "./styles/css-reset.css";
 import "./styles/modern-normalize.css";
+import DropdownStyles from './components/DropdownStyles';
+import Dropdown from './components/Dropdown';
 console.log("Hello World");
+
+const dropDown = new Dropdown();
+new DropdownStyles();
+dropDown.addContent({
+    link:'www.google.com',
+    text:'first',
+});
+dropDown.addContent({
+    link:'www.google.com',
+    text:'second',
+});
+dropDown.addContent({
+    link:'www.google.com',
+    text:'third',
+});
