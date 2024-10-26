a00f3371e02b2667b8960a90921d63b0f4e76c31
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-10-26 16:07:28 -0400

    add class method that allows the user to append or remove the dropdown to an existing element within the document

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,4 @@
-export default class Dropdown{
+export default class Dropdown {
     constructor(){
         this.parentDiv = document.createElement('div');
         this.parentDiv.classList.add('dropdown');
@@ -6,34 +6,65 @@ export default class Dropdown{
         this.button.classList.add('dropbtn');
         this.button.textContent='Dropdown';
         this.childDiv = document.createElement('div');
-        this.#buttonEventListener();
         this.childDiv.classList.add('dropdown-content');
         this.childDiv.id = 'myDropdown';
         this.parentDiv.appendChild(this.button);
         this.parentDiv.appendChild(this.childDiv);
-        this.docFrag = new DocumentFragment();
-        this.docFrag.appendChild(this.parentDiv);
-        this.body = document.querySelector('body');
-        this.body.appendChild(this.docFrag);
     }
-    #buttonEventListener(){
-        this.button.addEventListener('click',dropDownClick);
+    #buttonEventListener(action){
         function dropDownClick(event){
-            console.log(event.target.parentNode.lastElementChild);
+            let dropdowns = document.querySelectorAll('.dropdown-content');
+                dropdowns.forEach((element)=>{
+                    if(element.classList.contains('show')){
+                        element.classList.remove('show');
+                    }
+                });
             const myDropDown = event.target.parentNode.lastElementChild;
             myDropDown.classList.toggle("show");
         }
+        if(action === 'add'){
+            this.button.addEventListener('click',dropDownClick);
+        }
+        if(action === 'remove'){
+            this.button.removeEventListener('click',dropDownClick);
+        }
+    }
+    appendDropdownTo(parent){
+        this.#buttonEventListener('add');
+        if(parent !==''){
+            const dropdownLocation = document.querySelector(parent);
+            if(dropdownLocation){
+                dropdownLocation.appendChild(this.parentDiv);
+            }
+        }
+    }
+    removeDropdownFrom(parent){
+        this.#buttonEventListener('remove');
+        if(parent !==''){
+            const dropdownLocation = document.querySelector(parent);
+            if(dropdownLocation){
+                dropdownLocation.removeChild(this.parentDiv);
+            }
+        }
     }
     addContent(...content){
-        content.forEach((element)=>{
-            let {link} = element;
-            let {text} = element;
+        function createLink(link,text){
             let newContent = document.createElement('a');
             newContent.setAttribute('href',link);
             newContent.textContent = text;
-            this.docFrag.appendChild(newContent);
+            return newContent;
+        }
+        content.forEach((element)=>{
+            let {link} = element;
+            let {text} = element;
+            if(link && text){
+                this.childDiv.appendChild(createLink(link,text));
+            }
+            else if(!link && text){
+                this.childDiv.appendChild(createLink('',text));
+            }
         });
-        this.childDiv.appendChild(this.docFrag);
+        
     }
     setButtonText(...text){
         this.button.textContent='';
